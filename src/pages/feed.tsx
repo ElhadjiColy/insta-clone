f8d39875ec6021d6a2d60e559cb959061cdcbaf9
bamba <diagnec341@gmail.com>
2022-09-22 20:37:00 +0000

    Implement gallery logic and footer

--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -1,15 +1,34 @@
 import React from 'react'
+import Gallery from '../components/Gallery'
 import { Header } from '../components/Header'
 import { Search } from '../components/Search'
 
 export const Feed: React.FC = () => {
-    return <nav className="bg-slate-200 ">
+  const img1 =
+    'https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80'
+  const img2 =
+    'https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80'
+  const img3 =
+    'https://images.unsplash.com/photo-1622890806166-111d7f6c7c97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80'
+  const img4 =
+    'https://images.unsplash.com/photo-1590523277543-a94d2e4eb00b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80'
+  const img5 =
+    'https://images.unsplash.com/photo-1575424909138-46b05e5919ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80'
+
+  return (
+    <div>
+      <nav className="bg-slate-200 ">
         <div className="py-3 px-4 mx-auto max-w-screen-xl md:px-6">
-            <div className="flex justify-center items-center">
-                <Header>
-                    <Search/>
-                </Header>
-            </div>
+          <div className="flex justify-center items-center">
+            <Header>
+              <Search />
+            </Header>
+          </div>
         </div>
-    </nav>
+      </nav>
+      <div className="flex justify-center pt-70">
+        <Gallery galleries={[img1, img2, img3, img4, img5]} />
+      </div>
+    </div>
+  )
 }
