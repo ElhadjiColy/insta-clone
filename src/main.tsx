bc78bef87742f7df7dd3760563895318fa8f2b46
Nfally <nfallyibrahimacoly@gmail.com>
2022-09-22 20:13:48 +0000

    router handled

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,32 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom"
+import { Feed } from './pages/feed'
+import { Profile } from './pages/profile'
+import { Detail } from './pages/detail'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Feed/>,
+  },
+  {
+    path: "/profile",
+    element: <Profile/>,
+  },
+  {
+    path: "/detail",
+    element: <Detail/>,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
