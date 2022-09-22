0e1173e9b41919cf7c1c271edcd48ad4dfef8b3d
bamba <diagnec341@gmail.com>
2022-09-22 18:18:24 +0000

    Add logic for search

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
 export const Card: React.FC = () => {
-  return <div>Card</div>
+  return (
+    <div className="snap-start">
+      <img src="https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+    </div>
+  )
 }
